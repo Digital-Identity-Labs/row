a3e06755a825dcb12874fef1110f53d83f38a868
Pete Birkinshaw <pete@binary-ape.org>
2017-10-04 11:35:09 +0100

    Stop packaging/caching gems, use new location for gems, variable binstubs

--- optfs/admin/prepare_apps.sh
+++ optfs/admin/prepare_apps.sh
@@ -6,8 +6,10 @@ set -u
 ## Carry out all steps as one command, to return a failure code if something goes wrong
 chmod a+x /opt/admin/*.sh && \
 mkdir -p /opt/app/bin && \
+mkdir -p $BUNDLE_PATH && \
+mkdir -p /opt/app/vendor/cache && \
 mkdir -p /opt/app/tmp && chown -R app /opt/app/tmp && \
 mkdir -p /opt/app/log && chown -R app /opt/app/log && \
 bundle config --global silence_root_warning 1 && \
-bundle install --binstubs && \
+bundle install && bundle binstubs --force puma $RUBY_EXTRA_BINSTUBS && \
 sync
